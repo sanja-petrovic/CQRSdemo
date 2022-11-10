6862d162cd9a74b8947ade43b6094d2ba8268766
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-11-10 13:32:25 +0100

    Dockerized

---
+++ user/src/main/java/com/example/crqrsdemo/CrqrsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crqrsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrqrsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrqrsDemoApplication.class, args);
+	}
+
+}
---
+++ user/src/test/java/com/example/crqrsdemo/CrqrsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crqrsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrqrsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
