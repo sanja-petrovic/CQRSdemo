3c10592d6848da4e76685cf44d38c3997856835c
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-11-14 10:18:08 +0100

    User project setup

--- notification/src/main/java/com/example/notification/service/NotificationServiceImpl.java
+++ notification/src/main/java/com/example/notification/service/NotificationServiceImpl.java
@@ -1,4 +1,7 @@
 package com.example.notification.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationServiceImpl implements NotificationService {
 }
---
+++ user/src/main/java/com/example/user/model/User.java
@@ -0,0 +1,29 @@
+package com.example.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private UUID id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String email;
+}
